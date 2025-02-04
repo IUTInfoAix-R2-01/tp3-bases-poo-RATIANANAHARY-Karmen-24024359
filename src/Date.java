508daca561b1a950de9805224d380968f55bccc4
RATIANANAHARY-Karmen-24024359 <karmen.ratiananahary@etu.univ-amu.fr>
2025-02-04 17:29:37 +0100

    Exercice 1.7 pas terminé

---
+++ src/Date.java
@@ -0,0 +1,39 @@
+//Date
+public class Date {
+	private int day, month, year;
+	
+	public Account (int day, int month, int year) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+	
+	//Getters
+	public int getDay() {
+		 return day;
+	}
+	
+	public int getMonth() {
+		return month;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	//Setters
+	public void setDay(int day) {
+		this.day = day;
+	}
+	
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	
+	
+}
